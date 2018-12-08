569823f1bbdfdf3e860a806f5d24ee7b6534c096
Pedro Aguiar <pedro_deaguiar@yahoo.com.br>
2018-12-08 02:39:08 +0000

    Adding first example to

---
+++ main.tf
@@ -0,0 +1,33 @@
+resource "azurerm_kubernetes_cluster" "k8s" {
+    name                = "${var.cluster_name}"
+    location            = "${var.location}"
+    resource_group_name = "${var.azure_resource_group}"
+    dns_prefix          = "${var.dns_prefix}"
+
+    linux_profile {
+        admin_username = "${var.linux_profile_admin_username}"
+
+        ssh_key {
+        key_data = "${file("${var.linux_profile_ssh_key}")}"
+        }
+    }
+
+    agent_pool_profile {
+        name            = "${var.agent_pool_profile_name}"
+        count           = "${var.agent_pool_profile_count}"
+        vm_size         = "${var.agent_pool_profile_vm_size}"
+        os_type         = "${var.agent_pool_profile_os_type}"
+        os_disk_size_gb = "${var.agent_pool_profile_os_disk_size_gb}"
+        max_pods        = "${var.agent_pool_profile_max_pods}"
+        vnet_subnet_id  = "${var.agent_pool_profile_vnet_subnet_id}"
+    }
+
+    service_principal {
+        client_id     = "${var.service_principal_client_id}"
+        client_secret = "${var.service_principal_client_secret}"
+    }
+
+    tags {
+        Team = "${var.tag_team}"
+    }
+}
